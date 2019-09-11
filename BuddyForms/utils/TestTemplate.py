8225456acb5f43b0a2588dad8291bb3e8d36b8f0
Nery Marin <njota08@gmail.com>
2019-09-11 17:33:37 -0400

    - Established a way to run the suite from different operative systems (Windows, Linux and MacOS).

--- BuddyForms/utils/TestTemplate.py
+++ BuddyForms/utils/TestTemplate.py
@@ -1,15 +1,34 @@
+import platform
 import unittest
 import time
+from pathlib import Path
+
 from selenium import webdriver
 from BuddyForms.utils.Urls import Urls
 
 
 class TestTemplate(unittest.TestCase):
+    """ Operative System """
+    os = platform.system()
+
+    """ Webdriver Path """
+    if os == "Linux":
+        driver_path = "utils/chromedrivers/chromedriver-linux"
+    elif os == "Darwin":
+        driver_path = "utils/chromedrivers/chromedriver-mac"
+    elif os == "Windows":
+        driver_path = "utils/chromedrivers/chromedriver.exe"
 
     def setUp(self):
-        """ Modify the WebDriver location path if needed.
-        I'm looking for another option to avoid this """
-        self.driver = webdriver.Chrome("C:/Users/Njota/PycharmProjects/Themekraft/chromedriver.exe")
+        my_path = Path().absolute()
+        if my_path.name == "tests":
+            driver_location = my_path.parent / self.driver_path
+        elif my_path.name == "BuddyForms":
+            driver_location = my_path / self.driver_path
+        else:
+            driver_location = None
+
+        self.driver = webdriver.Chrome(driver_location)
         self.driver.maximize_window()
         self.driver.get(Urls.wordpress_admin_page_apache_auth)
         time.sleep(5)
